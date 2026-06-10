dc5e1eb0fd1c00b1c82c3cdb5a55ff25acb4f191
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-06-10 14:38:10 +0800

    feat: tasks menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,7 +27,9 @@
                             {{ __('Admin') }}
                         </x-nav-link>
 
-
+                        <x-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.*')">
+                            {{ __('Tasks') }}
+                        </x-nav-link>
 
                     @endauth
 
