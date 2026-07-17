5de63df3d58567fae09ad42d8c29e93e3b392b52
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-07-17 10:14:48 +0800

    improve task seeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Task;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class TaskSeeder extends Seeder
@@ -12,6 +13,37 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        Task::factory(10)->create();
+        $tasks = [
+            [
+                'label' => 'make quinoa',
+                'due' => fake()->date(),
+                'badge' => 2,
+            ],
+            [
+                'label' => 'finish AT2',
+                'due' => Carbon::now(),
+                'badge' => 1,
+            ],
+            [
+                'label' => 'redesign website',
+                'due' => null,
+                'badge' => 3,
+            ],
+
+        ];
+        foreach ($tasks as $task) {
+
+                        Task::updateOrCreate([
+                            'user_id' => 100,
+                            'label' => $task['label'],
+                            'completed' =>fake()->boolean(),
+                            'description' => fake()->words(5, true),
+                            'due' => $task['due'],
+                            'badge' => $task['badge'],
+            ]
+                        );
+
+        }
+
     }
 }
