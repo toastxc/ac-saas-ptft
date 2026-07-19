7bc7ea8b4381374a9f1ed4fd19fd3b3931c28b02
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-07-19 09:23:41 +0800

    replace logo with Home

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,14 +4,17 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <x-application-logo
-                            class="block h-9 w-auto fill-current text-laravel-500 hover:text-laravel-900"/>
-                    </a>
-                </div>
+{{--                <div class="shrink-0 flex items-center">--}}
+{{--                    <a href="{{ route('home') }}">--}}
+{{--                        {{ __('Tasks') }}--}}
+{{--                    </a>--}}
+{{--                </div>--}}
 
                 <!-- Navigation Links -->
+
+                <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                    {{ __('Home') }}
+                </x-nav-link>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
 
 
