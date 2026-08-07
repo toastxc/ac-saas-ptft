29383419c398698c9b21f09dee26410cd3d1e17d
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-08-07 09:13:22 +0800

    remove unused

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -2,19 +2,13 @@
 
 namespace App\Models;
 
-use Database\Factories\BadgeFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Badge extends Model
 {
-    /** @use HasFactory<BadgeFactory> */
-    use HasFactory;
-
-    use HasFactory;
 
     protected $fillable = [
-        'label',
-        'color',
+     'color'
     ];
 }
